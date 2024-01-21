e28a55d833b3eab86e8b4d73a8ec681e1f504afe
Kleber Molina Kardel <molinakardel@gmail.com>
2024-01-21 09:25:42 -0300

    :sparkles: feat: Cria configuração da rota para os usuários

---
+++ api/controllers/user.js
@@ -0,0 +1,20 @@
+// Importa o objeto 'db' do módulo 'db.js' para interação com o banco de dados
+import { db } from "../db.js";
+
+// Define a função 'getUsers' para lidar com a requisição de obtenção de usuários
+export const getUsers = (_, res) => {
+    // Cria uma string de consulta SQL para selecionar todos os registros da tabela 'usuarios'
+    const q = "SELECT * FROM usuarios";
+
+    // Executa a consulta no banco de dados usando o método 'query' do objeto 'db'
+    db.query(q, (err, data) => {
+        // Verifica se ocorreu algum erro durante a execução da consulta
+        if (err) {
+            // Se houver um erro, envia a resposta JSON contendo o erro
+            return res.json(err);
+        }
+
+        // Se a consulta foi bem-sucedida, envia a resposta JSON com os dados e um status HTTP 200 (OK)
+        return res.status(200).json(data);
+    });
+};
---
+++ api/routes/users.js
@@ -0,0 +1,15 @@
+// Importa o módulo 'express' para criar rotas
+import express from "express";
+
+// Importa a função 'getUsers' do arquivo 'user.js' no diretório 'controllers'
+import { getUsers } from "../controllers/user.js";
+
+// Cria um objeto router utilizando o método express.Router()
+const router = express.Router();
+
+// Define uma rota HTTP GET para a raiz ("/") da aplicação
+// Quando uma requisição GET é feita para a raiz, a função getUsers será chamada
+router.get("/", getUsers);
+
+// Exporta o objeto router para que possa ser utilizado em outros lugares do código
+export default router;
