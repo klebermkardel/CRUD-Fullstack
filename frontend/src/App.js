f4406ccf426d59f2cab23ed66432a82b9281fe76
Kleber Molina Kardel <molinakardel@gmail.com>
2024-01-21 11:21:45 -0300

    :sparkles: feat: Cria e estilização componente Container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,22 @@ import styled from "styled-components"
 import { toast, ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction column;
+  align-items: center;
+  gap: 10px;
+`
+
+
 function App() {
   return (
     <>
+      <Container>
+      </Container>
       <ToastContainer autoClose={3000} position={toast.POSTION.BOTTOM_LEFT} />
       <GlobalStyle />
     </>
