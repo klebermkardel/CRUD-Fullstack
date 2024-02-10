7e7813e75b8bf6acf1129af62feda626a4be2b43
Kleber Molina Kardel <molinakardel@gmail.com>
2024-02-10 07:36:28 -0300

    :sparkles: feat: Importa axios
    Importa axios para utilização da API

--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import styled from "styled-components";
 import { FaTrash, FaEdit } from "react-icons/fa";
 import { toast } from "react-toastify";
