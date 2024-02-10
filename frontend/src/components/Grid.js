d93b58aee8df0c4c60f5f01ef9e40634ee0a8d52
Kleber Molina Kardel <molinakardel@gmail.com>
2024-02-10 07:31:01 -0300

    :lipstick: feat: Estiliza componente Table
    Cria estilização da tabela de lista de usuários

--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -1,9 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
+const Table = styled.table`
+    width: 100%;
+    background-color: #fff;
+    padding: 20px;
+    box-shadow: 0px 0px 5px #ccc;
+    border-radius: 5px;
+    max-width: 800px;
+    margin: 20px auto;
+    word-break: breal-all;
+`;
+
 const Grid = () => {
     return (
-
+        <Table>
+            
+        </Table>
     );
 };
 
